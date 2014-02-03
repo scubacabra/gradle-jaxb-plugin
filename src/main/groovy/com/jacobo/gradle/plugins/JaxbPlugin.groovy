78ffacedb005ca1aa9ba89995f5db857a7a71917
Daniel Mijares <daniel.j.mijares@gmail.com>
2014-02-03 17:03:43 -0500

    no content change, just formatting for 80cols

--- src/main/groovy/com/jacobo/gradle/plugins/JaxbPlugin.groovy
+++ src/main/groovy/com/jacobo/gradle/plugins/JaxbPlugin.groovy
@@ -13,16 +13,22 @@ import com.jacobo.gradle.plugins.task.JaxbXJCTask
 import com.jacobo.gradle.plugins.JaxbExtension
 
 /**
- * A plugin used for taking a whole folder full of .xsd files and enabling separate compilation parsing the xsd files for @targetNamespace information, grouping unique namespaces and generating an
+ * A plugin used for taking a whole folder full of .xsd files and enabling
+ * separate compilation parsing the xsd files for @targetNamespace
+ * information, grouping unique namespaces and generating an
  * @see OrderGraph
  * that is then run through the jaxb xjc task for each namespace and associated
  * @see XsdNamespaces data
  * <p>
- * Declares a <tt>jaxb</tt> configuration which needs to be configured with the jaxb libraries to be used.
+ * Declares a <tt>jaxb</tt> configuration which needs to be configured with the
+ * jaxb libraries to be used.
  * <p>
  * Declares a <tt>xjc</tt> task to be executed in gradle.  
  * <p>
- * Each XsdNamespaces data runs through the task <tt>xjc</tt> and generates an episode file, all namespaces dependent on other namespaces re-use episode files, so that projects depending on different schema set can be grouped accordingly with no duplicate regeneration.
+ * Each XsdNamespaces data runs through the task <tt>xjc</tt> and generates an
+ * episode file, all namespaces dependent on other namespaces re-use episode
+ * files, so that projects depending on different schema set can be grouped
+ * accordingly with no duplicate regeneration.
  * @author Daniel Mijares
  * Created: Tue Dec 04 09:01:34 EST 2012
  * @see JaxbExtension
@@ -41,16 +47,17 @@ class JaxbPlugin implements Plugin<Project> {
     project.plugins.apply(JavaPlugin)
     configureJaxbExtension(project)
     configureJaxbNamespaceConfiguration(project)
-    JaxbNamespaceTask jnt = configureJaxbNamespaceDependencyGraph(project, extension)
+    JaxbNamespaceTask jnt = configureJaxbNamespaceDependencyGraph(project,
+								  extension)
     configureJaxbGenerateSchemas(project, extension, jnt)
   }
   
   private void configureJaxbExtension(final Project project) { 
     extension = project.extensions.create("jaxb", JaxbExtension, project)
     extension.with { 
-      jaxbSchemaDirectory = "${project.rootDir}/schema"
-      jaxbEpisodeDirectory = "${project.rootDir}/schema/episodes" 
-      jaxbBindingDirectory = "${project.rootDir}/schema/bindings"
+      jaxbSchemaDirectory = "schema"
+      jaxbEpisodeDirectory = "schema/episodes"
+      jaxbBindingDirectory = "schema/bindings"
       jaxbSchemaDestinationDirectory = "src/main/java"
       extension = 'true'
       removeOldOutput = 'yes'
@@ -65,18 +72,27 @@ class JaxbPlugin implements Plugin<Project> {
     }
   }
 
-  private JaxbNamespaceTask configureJaxbNamespaceDependencyGraph(final Project project, JaxbExtension jaxb) { 
-    JaxbNamespaceTask jnt = project.tasks.add(JAXB_NAMESPACE_GRAPH_TASK,  JaxbNamespaceTask)
-    jnt.description = "go through the ${jaxb.xsdDirectoryForGraph} folder and find all unique namespaces, create a namespace graph and parse in teh graph order with jaxb"
+  private JaxbNamespaceTask configureJaxbNamespaceDependencyGraph(
+    final Project project, JaxbExtension jaxb) {
+    JaxbNamespaceTask jnt = project.tasks.add(JAXB_NAMESPACE_GRAPH_TASK,
+					      JaxbNamespaceTask)
+    jnt.description = "go through the ${jaxb.xsdDirectoryForGraph} folder " +
+      "and find all unique namespaces, create a namespace graph and parse in " +
+      "the graph order with jaxb"
     jnt.group = JAXB_NAMESPACE_TASK_GROUP
     jnt.conventionMapping.xsdDirectory = {
       new File(project.rootDir, project.jaxb.jaxbSchemaDirectory) }
     return jnt
   }
 
-  private void configureJaxbGenerateSchemas(final Project project, JaxbExtension jaxb, JaxbNamespaceTask jnt) { 
-    JaxbXJCTask xjc = project.tasks.add(JAXB_NAMESPACE_GENERATE_TASK,  JaxbXJCTask)
-    xjc.description = "run through the Directory Graph for ${jaxb.xsdDirectoryForGraph} and parse all schemas in order generating episode files to ${jaxb.jaxbEpisodeDirectory}"
+  private void configureJaxbGenerateSchemas(final Project project,
+					    JaxbExtension jaxb,
+					    JaxbNamespaceTask jnt) {
+    JaxbXJCTask xjc = project.tasks.add(JAXB_NAMESPACE_GENERATE_TASK,
+					JaxbXJCTask)
+    xjc.description = "run through the Directory Graph for " +
+      "${jaxb.xsdDirectoryForGraph} and parse all schemas in order generating" +
+      " episode files to ${jaxb.jaxbEpisodeDirectory}"
     xjc.group = JAXB_NAMESPACE_TASK_GROUP
     xjc.dependsOn(jnt)
     xjc.conventionMapping.manager = {
