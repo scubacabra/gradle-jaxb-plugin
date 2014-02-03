237b87cc47b719ea321d6e58e630f61d50474cfc
Daniel Mijares <daniel.j.mijares@gmail.com>
2014-02-03 15:44:58 -0500

    cleaning up resolve document dependencies test.  Little FYI in the test that the documentDependencies variables isn't order gauranteed, hence the containsAll() test.  HashMap values give no order gaurantee, change to LinkedHashMap if that is the desire

--- src/test/groovy/com/jacobo/gradle/plugins/model/XsdSlurperSpec.groovy
+++ src/test/groovy/com/jacobo/gradle/plugins/model/XsdSlurperSpec.groovy
@@ -15,18 +15,27 @@ class XsdSlurperSpec extends BaseSpecification {
       def file = getFileFromResourcePath(filePath)
       slurper.slurpedDocument = new XmlSlurper().parse(file)
       slurper.documentFile = file
+      slurper.xsdNamespace = "someNamespace"
 
     when: "resolve the slurper dependencies"
       slurper.resolveDocumentDependencies()
 
     then: "get the certain imports and include"
-      slurper.xsdImports == imports as Set
-      slurper.xsdIncludes == includes as Set
-      slurper.documentDependencies == dependencyLocations.collect{ getFileFromResourcePath(it) } as Set
+      slurper.xsdImports == imports.collect{ getFileFromResourcePath(it) } as Set
+      slurper.xsdIncludes == includes.collect { getFileFromResourcePath(it) } as Set
+      // document dependencies not gauranteed to be ordered, LinkedHashMap if want order
+      slurper.documentDependencies.values().containsAll(
+	dependencyLocations.collect{ getFileFromResourcePath(it) } as Set
+      )
 
     where:
-      filePath				| imports		| includes				| dependencyLocations
-      "/schema/Include/OrderNumber.xsd"	| ["Product.xsd"]	| ["include.xsd", "include2.xsd"]	| ["/schema/Include/Product.xsd", "/schema/Include/include.xsd", "/schema/Include/include2.xsd"]
-      "/schema/Include/Product.xsd"	| []			| []					| []
+      filePath << ["/schema/Include/OrderNumber.xsd",
+		   "/schema/Include/Product.xsd"]
+      imports << [["/schema/Include/Product.xsd"], []]
+      includes << [ ["/schema/Include/include.xsd",
+		     "/schema/Include/include2.xsd"], []]
+      dependencyLocations << [["/schema/Include/Product.xsd",
+			      "/schema/Include/include.xsd",
+			      "/schema/Include/include2.xsd"], []]
   }
 }
