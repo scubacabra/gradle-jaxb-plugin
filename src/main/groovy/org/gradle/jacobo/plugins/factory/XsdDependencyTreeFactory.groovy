e1a4956a34c4f60b4e90f95015b84eb6d8f39ee1
jacobono <daniel.j.mijares@gmail.com>
2014-04-28 19:49:10 -0400

    XsdDependencyTreeFactory is a service object that takes a list of NamespaceData objects and a list of all the BaseSchemaDocument objects collected from the xsd files.
    Resolves dependent Namespaces to a map of NamespaceData => Set of
    namespaces (strings) depended on.  This is a shallow dependency, only
    what is specified in the xsd file is taken into consideration, no deep
    dependency resolution.
    
    Creating the dependency tree is simply a matter of figuring out which
    dependent namespaces (those namespaces that depend on others) meet
    certain criteria (operating on namespace strings associated with
    NamespaceData object)
    
    1. at least on depdendency needs to be in the 'current' tree row
    2. all depenendcies should be managed already
    
    aquire the namespaces that pass this criteria and send them to the
    TreeManager and keep going until there are no more dependent namespaces
    left to add to the tree.

---
+++ src/main/groovy/org/gradle/jacobo/plugins/factory/XsdDependencyTreeFactory.groovy
@@ -0,0 +1,65 @@
+package org.gradle.jacobo.plugins.factory
+
+import org.gradle.api.logging.Logging
+import org.gradle.api.logging.Logger
+
+import org.gradle.jacobo.schema.BaseSchemaDocument
+import org.gradle.jacobo.plugins.model.TreeManager
+import org.gradle.jacobo.plugins.structures.NamespaceData
+
+class XsdDependencyTreeFactory {
+  static final Logger log = Logging.getLogger(XsdDependencyTreeFactory.class)
+
+  TreeManager treeManager
+  
+  public XsdDependencyTreeFactory(TreeManager treeManager) {
+    this.treeManager = treeManager
+  }
+
+  public TreeManager createDependencyTree(List<NamespaceData> namespaces,
+					  List<BaseSchemaDocument> documents) {
+    log.lifecycle("jaxb: generating xsd namespace dependency tree")
+    treeManager.createTreeRoot(namespaces.findAll { it.hasDependencies == false })
+    def haveDependencies = namespaces.findAll { it.hasDependencies == true }
+    def namespaceDependencies = resolveDependentNamespaces(haveDependencies,
+							   documents)
+    while(!namespaceDependencies.isEmpty()) {
+      def managedNamespaces = treeManager.managedNodes.data.namespace
+      def currentRowNamespaces = treeManager.currentTreeRow.data.namespace
+      def meetsCriteria = namespaceDependencies.findAll {namespace, dependencies ->
+	// at least one dependency needs to be in the current Tree Row
+	if (!dependencies.any{ currentRowNamespaces.contains(it)} ) return false
+	// all dependencies should  be managed already
+	if (dependencies.any{ !managedNamespaces.contains(it)} ) return false
+	//meets criteria -- should be found
+	return true
+      }
+      meetsCriteria.each { namespaceDependencies.remove(it.key) }
+      treeManager.addChildren(meetsCriteria)
+    }
+
+    return treeManager
+  }
+
+  def Map<NamespaceData, Set<String>> resolveDependentNamespaces(
+    List<NamespaceData> namespaces, List<BaseSchemaDocument> documents) {
+    def namespaceDependencies = [:]
+    namespaces.each { namespace ->
+      def dependentNamespaces = [] as Set
+      def dependencies = []
+      namespace.documents.each { document ->
+	// could resolve a file of an external dependency
+	dependencies.addAll(document.findResolvedXsdImports())
+      }
+      dependencies.each { dependency ->
+	def document = documents.find { doc -> doc.documentFile == dependency}
+	// could not find dependency in documents because could be external
+	if (document) {
+	  dependentNamespaces << document.xsdNamespace	  
+	}
+      }
+      namespaceDependencies[namespace] = dependentNamespaces - namespace.externalDependencies
+    }
+    return namespaceDependencies
+  }
+}
