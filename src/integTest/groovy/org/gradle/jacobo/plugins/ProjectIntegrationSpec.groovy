362eef2732c48650203583df0bf0460db273082a
jacobono <daniel.j.mijares@gmail.com>
2014-04-30 18:24:59 -0400

    adding integration tests -- ensuring proper runtime functionality and guice binding.
    Even the Xjc ant task can be checked for runtime correctness ONLY.
    AntXjc has to be mocked, but it still runs through a full task
    execution (and dependent execution) to arrive at a mock that is checked
    for interaction.
    
    Haven't figured out how to test ant Builder yet....

---
+++ src/integTest/groovy/org/gradle/jacobo/plugins/ProjectIntegrationSpec.groovy
@@ -0,0 +1,34 @@
+package org.gradle.jacobo.plugins
+
+import org.gradle.testfixtures.ProjectBuilder
+import org.gradle.jacobo.plugins.JaxbPlugin
+import org.gradle.jacobo.plugins.ProjectTaskSpecification
+import org.gradle.jacobo.plugins.task.JaxbNamespaceTask
+import org.gradle.jacobo.plugins.task.JaxbXJCTask
+
+class ProjectIntegrationSpec extends ProjectTaskSpecification {
+  // Task names
+  def dependenyTreeTaskName = JaxbPlugin.JAXB_XSD_DEPENDENCY_TREE_TASK
+  def xjcTaskName = JaxbPlugin.JAXB_XJC_TASK
+
+  // tasks
+  def dependencyTreeTask
+  def xjcTask
+
+  // root project to add sub project too, like a real situation would consist of
+  def rootProject
+
+  def setRootProject(def rootDir) {
+    rootProject = ProjectBuilder.builder().withProjectDir(rootDir).build()
+  }
+  
+  def setSubProject(def rootProject, def projectName, def plugin) {
+    project = ProjectBuilder.builder().withName(projectName).withParent(rootProject).build()
+    project.apply(plugin: plugin)
+  }
+
+  def setupProjectTasks() {
+    dependencyTreeTask = project.tasks[dependenyTreeTaskName] as JaxbNamespaceTask
+    xjcTask = project.tasks[xjcTaskName] as JaxbXJCTask
+  }
+}
---
+++ src/integTest/groovy/org/gradle/jacobo/plugins/task/JaxbXjcTaskSpec.groovy
@@ -0,0 +1,29 @@
+package org.gradle.jacobo.plugins.task
+
+import org.gradle.jacobo.plugins.ProjectIntegrationSpec
+
+import org.gradle.jacobo.plugins.ant.AntXjc
+
+class JaxbXjcTaskSpec extends ProjectIntegrationSpec {
+
+  def xjc = Mock(AntXjc)
+
+  def setup() {
+    def rootDir = getFileFromResourcePath("/test-jaxb-plugin")
+    setRootProject(rootDir)
+    setSubProject(rootProject, "multi-parent-child-schema", "jaxb")
+    setupProjectTasks()
+  }  
+
+  def "attempt an xjc with a mock, just to check guice bindings and runtime or error free"() {
+    given: "mock ant executor and execute dependent tasks"
+    xjcTask.xjc = xjc
+    dependencyTreeTask.execute()
+    
+    when:
+    xjcTask.execute()
+
+    then:
+    9 * xjc.execute(*_)
+  }
+}
