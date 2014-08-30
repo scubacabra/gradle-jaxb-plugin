bb8dc128662b4314b8b8e31b85ef548d1b367c8f
Daniel Mijares <djmijares@monk.local>
2014-08-29 20:50:55 -0400

    additional args patch. Merge of pull request #5.

--- src/main/groovy/org/gradle/jacobo/plugins/extension/XjcExtension.groovy
+++ src/main/groovy/org/gradle/jacobo/plugins/extension/XjcExtension.groovy
@@ -1,12 +1,15 @@
 package org.gradle.jacobo.plugins.extension
-
-import org.gradle.api.Project
-
 /**
  * {@code JaxbPlugin}'s default settings and and conventions for the
  * {@code xjc} task.
  */
 class XjcExtension {
+
+  /**
+   * Specifies the classname to use for the xjc task. If specifiying a custom one, be sure to add
+   * it to the configuration classpath.
+   */
+  String taskClassname
 
   /**
    * Destination directory for the generated output from the {@code xjc} task.
@@ -40,5 +43,17 @@ class XjcExtension {
    * See <a href="https://jaxb.java.net/2.2.4/docs/xjcTask.html">jaxb ant task</a>
    */
   boolean header
+
+  /**
+   * 'package' argument for {@code xjc} task. If specified, generated code will be placed under this Java package.
+   * See <a href="https://jaxb.java.net/2.2.4/docs/xjcTask.html">jaxb ant task</a>
+   */
+  String generatePackage
+
+  /**
+   * optional args argument for {@code xjc} task.
+   * See <a href="https://jaxb.java.net/2.2.4/docs/xjcTask.html">jaxb ant task</a>
+   */
+  List<String> args = []
 
 }
